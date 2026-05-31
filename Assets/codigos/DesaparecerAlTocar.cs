4f12e93ecc8e3d0abfc91e0acae8fe39e7078af3
Simon JM | Fullstack in progress <moncayosimon35@gmail.com>
2026-05-31 00:04:10 -0500

    probs desaparecen al ser tocados por harry

---
+++ Assets/codigos/DesaparecerAlTocar.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class DesaparecerAlTocar : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
